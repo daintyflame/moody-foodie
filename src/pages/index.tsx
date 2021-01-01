d583553268c528b158d80abdecb8bc52dcfd63d7
Vlady Veselinov <vladislav.veselinov@fanduel.com>
2021-01-01 01:00:23 +0000

    initial setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Moody Foodie<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@moodyfoodie</span>
+          <h2>Delicious fööd content from Sisi Atanasova</h2>
           <SocialList />
         </div>
       </div>
